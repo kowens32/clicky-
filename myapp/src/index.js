40d6a1598e733124f9e7c142484e0084ea2144e2
Kimberly <kowens4@elon.edu>
2018-01-03 16:16:50 -0500

    trying to capture the state of the images

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -20,23 +20,23 @@ class Board extends React.Component {
         }
     }
 
-
     handleClick(i) {
+
         const images = this.state.images.slice();
-        images[i] = 'X';
+        images[i] = 'clicked';
         this.setState({images: images});
     }
 
     renderSquare(i) {
-
         let array = ['BellatrixLestrange', 'DracoMalfoy', 'GinnyWeasley', 'HermioneGranger', 'LordVoldemort', 'NymphadoraLupin',
             'ProfessorAlbusDumbledore', 'ProfessorMinervaMcGonagall', 'ProfessorSeverusSnape', 'RonWeasley', 'RubeusHagrid', 'SiriusBlack']
         let images = array.map(image => {
-            return <img key={image} src={require(`./img/${image}.jpeg`)} alt="" className="img-responsive" />
+            return <img key={image} src={require(`./img/${image}.jpeg`)} alt="" className="img-responsive"
+
+            />
         });
 
         return (<Square
-
                 value={images[i]}
                 onClick={() => this.handleClick(i)}/>
         );
@@ -45,40 +45,35 @@ class Board extends React.Component {
 
     render() {
         const status = 'Click an image to start!';
-       //
-       //  let array = ['BellatrixLestrange', 'DracoMalfoy', 'GinnyWeasley', 'HermioneGranger', 'LordVoldemort', 'NymphadoraLupin',
-       //      'ProfessorAlbusDumbledore', 'ProfessorMinervaMcGonagall', 'ProfessorSeverusSnape', 'RonWeasley', 'RubeusHagrid', 'SiriusBlack']
-       // let images = array.map(image => {
-       //     return <img key={image} src={require(`./img/${image}.jpeg`)} alt="" className="img-responsive" />
-       // });
+
         return (
             <div>
-        <div className="status">{status}</div>
-            <div className="board-row">
-            {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}{this.renderSquare(3)}
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}{this.renderSquare(3)}
+
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(4)}{this.renderSquare(5)}{this.renderSquare(6)}{this.renderSquare(7)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(8)}{this.renderSquare(9)}{this.renderSquare(10)}{this.renderSquare(11)}
+                </div>
 
             </div>
-        <div className="board-row">
-          {this.renderSquare(4)}{this.renderSquare(5)}{this.renderSquare(6)}{this.renderSquare(7)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(8)}{this.renderSquare(9)}{this.renderSquare(10)}{this.renderSquare(11)}
-        </div>
-
-        </div>
         );
-   }
+    }
 
-       }
+}
 class Game extends React.Component {
     render () {
         return (
             <div className="game">
-            <div className="game-board">
-                <Board />
-            </div>
-                <div className="game-info"> </div>
+                <div className="game-board">
+                    <Board />
                 </div>
+                <div className="game-info"> </div>
+            </div>
         );
     }
 }
